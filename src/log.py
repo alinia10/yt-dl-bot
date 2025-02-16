444ec068f1e9df596feeb96c531e6fe263dcd7ad
Mohsen <91451917+mohsenahmadi2003@users.noreply.github.com>
2025-02-16 19:44:44 +0000

    [update]: check result foler exist

--- src/log.py
+++ src/log.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from datetime import datetime
 
 from jsonformatter import JsonFormatter  # type: ignore
@@ -15,20 +16,32 @@ class LoggingLevel:
     
 
 def get_logger():
-    # create a custom logger
-    logger = logging.getLogger(LOGGER_NAME)
-    if logger.handlers:  # logger is already setup, don't setup again
-        return logger
-    logger.propagate = False
-    logger.setLevel(logging.INFO)
+    # Check if 'results' folder exists, if not, create it
+    results_folder = "./results"
+    if not os.path.exists(results_folder):
+        os.makedirs(results_folder)  # Create the folder if it doesn't exist
 
-    file_handler = logging.FileHandler("./results/dl.log", encoding="utf8")
+    # Create a custom logger
+    logger = logging.getLogger(LOGGER_NAME)
+    if logger.handlers:  # If the logger has handlers, it is already set up
+        return logger
+    logger.propagate = False  # Disable propagation of log messages to parent loggers
+    logger.setLevel(logging.INFO)  # Set default log level to INFO
+
+    # Create file handler to log to a file within 'results' folder
+    file_handler = logging.FileHandler(f"{results_folder}/dl.log", encoding="utf8")
+    
+    # Create stream handler to log to console
     stream_handler = logging.StreamHandler()
+
+    # Set the format of the log messages to JSON format
     formatter = JsonFormatter(
-        ensure_ascii=False,
-        mix_extra=True,
-        mix_extra_position="head",
+        ensure_ascii=False,  # Allow non-ASCII characters
+        mix_extra=True,  # Include extra fields in the log output
+        mix_extra_position="head",  # Place extra fields at the beginning
     )
+
+    # Apply formatter to handlers
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
@@ -36,12 +49,12 @@ def get_logger():
 
 
 def simple_logger(message, log_level=LoggingLevel.INFO):
+    # Get the logger and log the message
     logger = get_logger()
     logger.log(
-        log_level,
-        message,
-        extra={
-            "logtime": datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
+        log_level,  # Log message with specified level
+        message,  # The actual log message
+        extra={  # Include additional information in the log entry
+            "logtime": datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),  # Include timestamp in the log
         },
     )
-    
